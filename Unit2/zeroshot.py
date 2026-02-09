f64d5859a3c3ff731bd0463f3ee10edeea52fbab
sparktalestrainings-sketch <sparktalestrainings@gmail.com>
2026-02-09 21:03:31 +0530

    Add files via upload

---
+++ Unit2/zeroshot.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+prompt = "Explain cloud computing in simple words"
+
+response = client.chat.completions.create(
+    model="llama-3.3-70b-versatile",
+    messages=[{"role": "user", "content": prompt}],
+    temperature=0.3
+)
+
+print(response.choices[0].message.content)
