9f609a6b636b584d1d04fd481c25143937aea981
sparktalestrainings-sketch <sparktalestrainings@gmail.com>
2026-02-09 20:59:26 +0530

    Update fewshotexample.py

--- Unit2/fewshotexample.py
+++ Unit2/fewshotexample.py
@@ -1,13 +1,10 @@
 import os
 from dotenv import load_dotenv
 from groq import Groq
-from openai import OpenAI
+
 load_dotenv()
-#client = Groq(api_key="GROQ_API_KEY")
-client = OpenAI(
-    base_url="https://api.groq.com/openai/v1", # Point to Groq
-    api_key=os.getenv("GROQ_API_KEY")
-)
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
 msgs = [
     {"role": "user", "content": "Q: What is AI? A: AI is the ability of machines to think like humans."},
     {"role": "user", "content": "Q: What is ML? A:"}
